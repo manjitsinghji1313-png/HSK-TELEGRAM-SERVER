0580c609ba81e4e153ce238a67a2d94dc19c5bce
Manjit Singh <manjitsinghji1313@gmail.com>
2026-08-08 10:13:58 +0530

    Update Dhan access token

--- HSK-BRAHMASTRA/config/supabase.js
+++ HSK-BRAHMASTRA/config/supabase.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 
 const { createClient } = require("@supabase/supabase-js");
 
-
-
 const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+
+console.log("================================");
+console.log("🔌 SUPABASE CONFIG");
+console.log("URL EXISTS :", !!process.env.SUPABASE_URL);
+console.log(
+    "SERVICE KEY EXISTS :",
+    !!process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+console.log("================================");
 
 module.exports = supabase;
