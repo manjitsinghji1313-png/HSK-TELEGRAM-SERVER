54dfe838e45cfabec3f1751d42558e726ac00b56
Manjit Singh <manjitsinghji1313@gmail.com>
2026-08-16 11:46:52 +0530

    Add Dhan config diagnostics

--- HSK-NORMAL-ORDER/config/config.js
+++ HSK-NORMAL-ORDER/config/config.js
@@ -7,3 +7,15 @@ module.exports = {
     ACCESS_TOKEN: process.env.DHAN_ACCESS_TOKEN
 
 };
+
+console.log("================================");
+console.log("🔐 DHAN CONFIG CHECK");
+console.log(
+    "CLIENT ID PRESENT :",
+    !!process.env.DHAN_CLIENT_ID
+);
+console.log(
+    "ACCESS TOKEN PRESENT :",
+    !!process.env.DHAN_ACCESS_TOKEN
+);
+console.log("================================");
